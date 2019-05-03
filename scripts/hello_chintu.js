e49818a8b8add276f0c183f52a1f21641f667f52
CHINTU KUMAR <41631483+2ch2@users.noreply.github.com>
2019-05-04 01:02:11 +0530

    hello_chintu.js

---
+++ scripts/hello_chintu.js
@@ -0,0 +1,6 @@
+// LANGUAGE: JavaScript
+// ENV: Node.js
+// AUTHOR: Chintu_Kumar
+// GITHUB: https://github.com/2ch2
+
+console.log(“Hello World! I’m Chintu kumar, a new stuff learner and Open Source Enthusiast.”);
